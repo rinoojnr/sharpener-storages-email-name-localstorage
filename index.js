7c01fd0e874e9054befc737b5b001e59e4da1d5b
rinoojnr <rinoojnr@gmail.com>
2023-10-08 12:12:10 +0530

    Added edit button and functionalies

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var itemList =document.getElementById("items");
 // console.log(localStorage.getItem("myObj"))  it gives [obj,obj] not proper
 form.addEventListener('submit',addStorage);
 itemList.addEventListener('click',removeLi);
+itemList.addEventListener('click',editLi);
 
 
 
@@ -17,6 +18,7 @@ function addStorage(e){
     e.preventDefault();
     var li =document.createElement('li')
     li.className="list-group-item"
+
   
     var text=document.createTextNode(nameList.value);
     var text1=document.createTextNode(emailList.value);
@@ -24,10 +26,14 @@ function addStorage(e){
     let button = document.createElement('button')
     button.className="btn-sm float-right delete";
     button.appendChild(document.createTextNode("X"))
+    var edit =document.createElement('button')
+    edit.className="btn-sm float-right edit"
+    edit.appendChild(document.createTextNode("Edit"))
     li.appendChild(text)
     li.appendChild(text2)
     li.appendChild(text1)
     li.appendChild(button)
+    li.append(edit)
     itemList.appendChild(li)
     let myObj ={
         name:nameList.value,
@@ -49,12 +55,25 @@ function addStorage(e){
 }
 function removeLi(e){
     if(e.target.classList.contains('delete')){
-        if(confirm("Are you sure")){
+        if(confirm("Are you sure to delete")){
             var rem = e.target.parentElement;
             itemList.removeChild(rem);
             console.log(rem)
             em_rem = rem.childNodes[2].textContent;
             localStorage.removeItem(em_rem)
+        }
+    }
+}
+function editLi(e){
+    if(e.target.classList.contains('edit')){
+        if(confirm("Are you sure to edit")){
+            var rem = e.target.parentElement;
+            itemList.removeChild(rem);
+            nameList.value = rem.childNodes[0].textContent;
+            emailList.value = rem.childNodes[2].textContent;
+            em_rem = rem.childNodes[2].textContent;
+            localStorage.removeItem(em_rem)
+
         }
     }
 }

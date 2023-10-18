b6da2768ba6873a5c4da092d8c7d36ef350103db
rinoojnr <rinoojnr@gmail.com>
2023-10-18 18:33:10 +0530

    Added POST method to store

--- index.js
+++ index.js
@@ -3,25 +3,67 @@ var nameList =document.getElementById('name')
 var emailList =document.getElementById('email')
 var submit =document.getElementById('submit')
 var itemList =document.getElementById("items");
+var phoneList =document.getElementById("phone")
 
 
 
 // console.log(localStorage.getItem("myObj"))  it gives [obj,obj] not proper
 form.addEventListener('submit',addStorage);
+
 itemList.addEventListener('click',removeLi);
 itemList.addEventListener('click',editLi);
 
 
 
-
-function addStorage(e){
-    e.preventDefault();
-    var li =document.createElement('li')
-    li.className="list-group-item"
+// function addStorage(e){
+//     e.preventDefault();
+//     var li =document.createElement('li')
+//     li.className="list-group-item"
 
   
-    var text=document.createTextNode(nameList.value);
-    var text1=document.createTextNode(emailList.value);
+//     var text=document.createTextNode(nameList.value);
+//     var text1=document.createTextNode(emailList.value);
+//     var text2=document.createTextNode(" - ");
+//     let button = document.createElement('button')
+//     button.className="btn-sm float-right delete";
+//     button.appendChild(document.createTextNode("X"))
+//     var edit =document.createElement('button')
+//     edit.className="btn-sm float-right edit"
+//     edit.appendChild(document.createTextNode("Edit"))
+//     li.appendChild(text)
+//     li.appendChild(text2)
+//     li.appendChild(text1)
+//     li.appendChild(button)
+//     li.append(edit)
+//     itemList.appendChild(li)
+//     let myObj ={
+//         name:nameList.value,
+//         email:emailList.value,
+//         phone:phoneList.value
+//     }
+//     // let myObj1 ={
+//     //     mname:"raju",
+//     //     email:"ramaraqju2123"
+//     // }
+//     // let myObj1_serialized = JSON.stringify(myObj1)
+//     // let myObj1_deserialized = JSON.parse(myObj1_serialized)
+
+//     let myobj_serialized = JSON.stringify(myObj)
+//     // localStorage.setItem("myObj",myobj_serialized)
+//     // localStorage.setItem('name',nameList.value);
+//     // localStorage.setItem('mail',emailList.value)
+//     localStorage.setItem(emailList.value,myobj_serialized)
+
+// }
+
+function showOnscreen(e){
+    
+    var li =document.createElement('li')
+    li.className="list-group-item"
+   
+  
+    var text=document.createTextNode(e.name);
+    var text1=document.createTextNode(e.email);
     var text2=document.createTextNode(" - ");
     let button = document.createElement('button')
     button.className="btn-sm float-right delete";
@@ -37,22 +79,31 @@ function addStorage(e){
     itemList.appendChild(li)
     let myObj ={
         name:nameList.value,
-        email:emailList.value
+        email:emailList.value,
+        phone:phoneList.value
     }
-    // let myObj1 ={
-    //     mname:"raju",
-    //     email:"ramaraqju2123"
-    // }
-    // let myObj1_serialized = JSON.stringify(myObj1)
-    // let myObj1_deserialized = JSON.parse(myObj1_serialized)
+}
 
-    let myobj_serialized = JSON.stringify(myObj)
-    localStorage.setItem("myObj",myobj_serialized)
-    localStorage.setItem('name',nameList.value);
-    localStorage.setItem('mail',emailList.value)
-    localStorage.setItem(emailList.value,myobj_serialized)
+
+
+
+function addStorage(e){
+    e.preventDefault()
+    const myObj ={
+        name:nameList.value,
+        email:emailList.value,
+        phone:phoneList.value
+    }
+    
+axios.post('https://crudcrud.com/api/7604fb59ea1648b88af96a1485bdc8e0/Appointment',myObj).then((res)=>{
+console.log(res)
+showOnscreen(res.data)
+}).catch((error)=>console.log("found error",error))        
 
 }
+
+
+
 function removeLi(e){
     if(e.target.classList.contains('delete')){
         if(confirm("Are you sure to delete")){
